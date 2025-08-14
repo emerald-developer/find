1251599cb0aab6089b7dd224d880c145fd5321da
emerald <emerald.developer@proton.me>
2025-08-14 21:17:33 +0530

    list cli

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,40 @@
+use clap::Parser;
 use std::fs::*;
+use std::path::Path;
 
-fn list_files(path: &str) -> Vec<String> {
-    let mut out: Vec<String> = Vec::new();
-    if let Ok(enteries) = read_dir(path) {
-        for entry in enteries {
-            match entry {
-                Ok(entry) => {
-                    let path = entry.path();
-                    if path.is_file() {
-                        out.push(path.to_string_lossy().into_owned());
-                    } else if path.is_dir() {
-                        out.extend(list_files(&path.to_string_lossy()));
-                    }
-                }
-                Err(err) => {
-                    eprintln!("Error reading directory entry: {}", err);
-                }
-            }
-        }
-    }
-    out
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    path: String,
 }
 
-fn main() {
-    println!("Hello, world!");
-    let list: Vec<String> = list_files("./test");
-    println!("{:#?}", list);
+fn list_files(path: &Path) -> Result<Vec<String>, std::io::Error> {
+    let mut out: Vec<String> = Vec::new();
+    for entry in read_dir(path)? {
+        let entry = entry?;
+        let path = entry.path();
+        if path.is_file() {
+            out.push(path.to_string_lossy().into_owned());
+        } else if path.is_dir() {
+            out.extend(list_files(&path)?);
+        }
+    }
+    Ok(out)
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+    let path = Path::new(&args.path);
+
+    if !path.exists() {
+        eprintln!("Error: Path '{}' does not exist.", args.path);
+        return Err("Path does not exist".into());
+    }
+
+    match list_files(path) {
+        Ok(list) => println!("{:#?}", list),
+        Err(err) => eprintln!("Error listing files: {}", err),
+    }
+    Ok(())
 }
